29a311ab5303f94471421efc2f1fd5af7bed8eca
kkami <kkamidog@naver.com>
2024-04-10 00:29:02 +0900

    Overlapping api communication

--- src/pages/ProductDetail/index.jsx
+++ src/pages/ProductDetail/index.jsx
@@ -1,13 +1,33 @@
 // import React from 'react';
 
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
   const { id } = useParams();
+  const [productDetail, setProductDetail] = useState({});
+
+  const getProductDetail = async () => {
+    const url = `https://my-json-server.typicode.com/kkaminipin/H-M/products/${id}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    setProductDetail(data);
+  };
+
+  useEffect(() => {
+    getProductDetail();
+  }, []);
 
   return (
     <div>
       <h1>상세보기 페이지입니다.</h1>
+      <ul>
+        <li>
+          <h3>{productDetail?.title}</h3>
+          <img src={productDetail?.img} alt={productDetail?.title} />
+          <p>가격 : {productDetail?.price}</p>
+        </li>
+      </ul>
     </div>
   );
 };
--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -4,13 +4,13 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Products = () => {
-  const [productsList, setProductsList] = useState([]);
+  const [productList, setProductList] = useState([]);
 
   const getProducts = async () => {
     const url = 'https://my-json-server.typicode.com/kkaminipin/H-M/products';
     const response = await fetch(url);
     const data = await response.json();
-    setProductsList(data);
+    setProductList(data);
   };
 
   useEffect(() => {
@@ -21,12 +21,12 @@ const Products = () => {
     <div>
       <h1>상품들 소개</h1>
       <ul>
-        {productsList.map((product) => {
+        {productList.map((product) => {
           return (
-            <li key={product.id}>
-              <Link to={`${product.id}`}>
+            <li key={product?.id}>
+              <Link to={`${product?.id}`}>
                 <div>
-                  <h3>{product.title}</h3>
+                  <h3>{product?.title}</h3>
                 </div>
               </Link>
             </li>
